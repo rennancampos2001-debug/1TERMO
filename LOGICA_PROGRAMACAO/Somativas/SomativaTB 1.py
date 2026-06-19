3c5324c133fd7dfa77a3ae009ca0586f53ab2ab4
Rennan <rennancampos2001@gmail.com>
2026-06-19 19:49:25 -0300

    Somativa 1 incompleta

---
+++ LOGICA_PROGRAMACAO/Somativas/SomativaTB 1.py
@@ -0,0 +1,62 @@
+# Atividade 1
+
+# nome = input("Qual é o seu nome ? ")
+# turno = input("você faz parte do turno A, B ou C ? ")
+
+# print(f"Operador {nome} registrado no Turno {turno}. Boa jornada!")
+
+# Atividade 2 
+
+# pecas_hora = int(input("Quantas peças foram produzidas em 1 hora ? "))
+
+# calculo = pecas_hora * 8
+
+# print(f"A quantidade de peças em 8 horas será de: {calculo}")
+
+# Atividade 3
+
+# pressao_bar = float(input("Qual a pressão em  Bar : "))
+
+# conversao = pressao_bar * 14.5
+
+# print(f"A pressão em PSI é de: {conversao}")
+
+# Atividade 4
+
+# nota1 = float(input("Qual é a primeira nota: "))
+# nota2 = float(input("Qual é a segunda nota: "))
+# nota3 = float(input("Qual é a terceira nota: "))
+
+# media = nota1 + nota2 + nota3 / 3
+
+# print(f"A media das notas é de: {media:.2f}")
+
+# Atividade 5 
+
+# temperatura = float(input("Qual é a temperatura do motor ? "))
+
+# if temperatura < 40:
+#     print("Baixa carga")
+# elif temperatura <= 70:
+#     print("Normal")
+# else:
+#     print("Alerta:Resfriamento Ativado!")
+
+# Atividade 6 
+
+# codigo = input("Qual é o código do produto? ").upper()
+
+# if codigo.startswith("A"):
+#     print("Alimentos")
+# elif codigo.startswith("E"):
+#     print("Eletrônicos")
+# else:
+#     print("Desconhecido")
+
+# porta = input("A porta está fechada ? (Sim/Não) ").lower()
+# botao = input("O botão de emergencia esta ligado ? (Sim/Não) ").lower()
+
+# if porta == "sim" and botao == "nao":
+#     print("Maquina iniciada")
+# else:
+#     print("A maquina não pode seer iniciada")
