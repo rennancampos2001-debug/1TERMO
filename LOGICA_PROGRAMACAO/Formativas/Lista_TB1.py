9f04f325be2e31ef10e413c346459bb625dbb24c
Rennan <rennancampos2001@gmail.com>
2026-06-19 18:37:07 -0300

    Formativa 1

---
+++ LOGICA_PROGRAMACAO/Formativas/Lista_TB1.py
@@ -0,0 +1,93 @@
+# Atividade 1
+
+# print("Bem-vindo ao mundo da programação em Python!")
+
+# Atividade 2
+
+# print("Rennan Campos Vieira \n" "16")
+
+# Atividade 3 (Sem Variaveis)
+
+# print(135 + 246)
+# print(512 - 128)
+
+# Atividade 3 (Com Variaveis)
+
+# def soma_escolha():
+
+#     valor1 = float(input("Digite o primeiro numero da soma: "))
+#     valor2 = float(input("Digite o seguno valor da soma: "))
+
+#     soma = valor1 + valor2
+
+#     print("O valor da soma é de: ", soma)
+
+# def subtracao_escolha():
+
+#     valor1 = float(input("Digite o primeiro numero da subtração: "))
+#     valor2 = float(input("Digite o seguno valor da subtração: "))
+
+#     subtracao = valor1 - valor2
+
+#     print("O resultado da subtração é de: ", subtracao)
+
+# escolha = input("Digite 'soma' ou 'subtracao': ")
+
+# if escolha == "soma":
+#     soma_escolha()
+# elif escolha == "subtracao":
+#     subtracao_escolha()
+# else:
+#     print("Opção inválida!")
+
+# Atividade 4 
+
+# print(15 * 8)
+# print(78 / 3)
+
+# Atividade 5
+
+# print(5**3)
+
+# Atividade 5
+
+# nome = "Rennan"
+# sobrenome = "Campos"
+
+# nome_completo = nome + " " + sobrenome
+
+# print(nome_completo)
+
+# Atividade 7
+
+# pecas_totais = float(input("Qual a quantidade de peças fabricadas ? "))
+# pecas_defeituosas = float(input("Qual é a quantidade de peças defeituosas ? "))
+
+# pecas_boas = pecas_totais - pecas_defeituosas
+
+# print("A taxa de aproveitamento foi de: ", pecas_boas/pecas_totais)
+
+# Atividade 8
+
+# print("Eu tenho 16 anos e, em 10 anos, terei", 16 + 10, "anos")
+
+# Atividade 9
+
+# custo_hotel = 250.50
+# custo_passagem = 412.00
+# noites  = int(input("Quantas noites você ira passar no hotel ? "))
+
+# custo_total = (custo_hotel * noites) + custo_passagem
+
+# print(f"O total da viagem ficara em: {custo_total}")
+
+# Atividade 10
+
+# print("==========================")
+# print("    Relatório de Vendas")
+# print("==========================")
+# print("  Produto:Notebook Gamer")
+# print("   Quantidade vendida:15")
+# print("  Preço unitário:R$5499.50")
+# print(" Total de vendas:R$82492.50")
+# print("==========================")
